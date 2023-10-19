f7c79689bd393bb8ba96f5da2d456d82aeec4258
KIMDAHYE3312 <rlaekgp331299@gmail.com>
2023-10-19 18:40:58 +0900

    commit

---
+++ practice/src/main/java/Example/ResultBill.java
@@ -0,0 +1,10 @@
+package Example;
+
+public class ResultBill {
+    static int total_money;
+    static int point_money;
+    public static void main(String[] args) {
+        PaymentController pc=new PaymentController();
+        pc.resultItem();
+    }
+}
